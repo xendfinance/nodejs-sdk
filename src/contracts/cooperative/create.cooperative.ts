4a4ae31d899d7dd6ce3cc0fe15a12cd182291f8e
Njoku Emmanuel <kalunjoku123@gmail.com>
2021-02-23 01:19:13 +0100

    implementing cooperative cycles

---
+++ src/contracts/cooperative/create.cooperative.ts
@@ -0,0 +1,38 @@
+import web3 from 'web3';
+import createContract from "../create.contract";
+import XendFinanceGroup from '../abis/XendFinanceGroup_Yearn_V1.json';
+import { COOPERATIVE } from '../addresses/localhost';
+import serializedSignedTransaction from "../../utils/sendSignedTransaction";
+
+type CooperativeCycle = {
+  provider: string
+  privateKey: string
+  groupId: number
+  cycleStakeAmount: any
+  payoutIntevalSeconds: number
+  startTimeInSeconds: number
+  maxMembers: number
+}
+
+export default async function (args: CooperativeCycle) {
+
+  let { privateKey, provider, groupId, cycleStakeAmount, payoutIntevalSeconds, startTimeInSeconds, maxMembers } = args;
+
+  //
+  try {
+
+    const contract = await createContract(provider, XendFinanceGroup.abi, COOPERATIVE.YEARN_GROUP);
+
+    cycleStakeAmount = web3.utils.toWei(cycleStakeAmount, 'ether'); // convert to big number
+
+    const data = await contract.methods.createCycle(groupId, startTimeInSeconds, payoutIntevalSeconds, maxMembers, false, cycleStakeAmount).encodeABI();
+
+    const signedTx = await serializedSignedTransaction(data, COOPERATIVE.YEARN_GROUP, privateKey, provider);
+
+    return signedTx;
+
+  } catch (err) {
+    console.error(err);
+    return {}
+  }
+}
